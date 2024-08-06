f6a6c09f992f8e4597cd5552431f0afbf1155203
Guilherme Paixão <guiferpa@gmail.com>
2024-08-06 13:54:10 -0300

    added vagrantfile for rust environment

---
+++ vagrant/boxes/rust-debian/provisioning.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ sudo apt-get update -y
+
+ ## Git ##
+ echo '###Installing Git..'
+ sudo apt-get install git -y
+
+ ## Neovim ##
+ echo '###Installing Neovim..'
+ sudo apt-get install neovim -y
+
